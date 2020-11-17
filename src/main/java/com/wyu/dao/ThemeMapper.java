791f80b3021a0eb54f217f113a5f6192e446a7d4
LeeLrd <769436227@qq.com>
2020-11-18 01:38:42 +0800

    加了图书方法(有几个未测试)

--- src/main/java/com/wyu/dao/ThemeMapper.java
+++ src/main/java/com/wyu/dao/ThemeMapper.java
@@ -3,16 +3,41 @@ package com.wyu.dao;
 import java.util.List;
 
 import com.wyu.entity.Theme;
-
+/**
+ * 
+ * @author 李润东
+ * @since 2020/11/17
+ *
+ */
 public interface ThemeMapper {
-	
+	/**
+	 * @apiNote 查询所有主题信息
+	 * @return
+	 */
 	public List<Theme> queryAll();
-	
+	/**
+	 * @apiNote 通过id查询主题信息
+	 * @param id
+	 * @return
+	 */
 	public Theme queryById(int id);
-	
+	/**
+	 * @apiNote 新增一个主题信息
+	 * @param theme
+	 * @return
+	 */
 	public int newTheme(Theme theme);
-	
-	public int deleteById();
-	
+	/**
+	 * @apiNote 通过id删除一个主题信息
+	 * @param id
+	 * @return
+	 */
+	public int deleteById(int id);
+	/**
+	 * @apiNote 更新一个主题信息
+	 * @param theme
+	 * @param themeName
+	 * @return
+	 */
 	public int updateById(Theme theme,String themeName);
 }
