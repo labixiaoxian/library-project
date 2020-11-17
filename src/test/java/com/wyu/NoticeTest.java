d2bda976ff8990bbf3e49df9d92ae52e360e7585
Lidacheng <lidacheng666@outlook.com>
2020-11-18 01:54:28 +0800

    增加了swagger依赖，解决了swagger的小bug，大体上写好Notice的Controller,修改NoticeMapper和NoticeService

--- src/test/java/com/wyu/NoticeTest.java
+++ src/test/java/com/wyu/NoticeTest.java
@@ -1,6 +1,5 @@
 package com.wyu;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -11,10 +10,10 @@ public class NoticeTest {
     @Autowired
     NoticeService noticeService;
 
-    @Test
-    public void testInsert() {
-        for (int i = 0; i < 100; i++) {
-            noticeService.insert("标题" + i, "内容" + i);
-        }
-    }
+//    @Test
+//    public void testInsert() {
+//        for (int i = 0; i < 100; i++) {
+//            noticeService.insert("标题" + i, "内容" + i);
+//        }
+//    }
 }
