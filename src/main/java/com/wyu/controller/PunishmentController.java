603f06af1121c30350aa3456da12b08fcf7d4256
DaCheng Li <lidacheng666@outlook.com>
2020-11-18 18:34:22 +0800

    2020/11/18 18:34

--- src/main/java/com/wyu/controller/PunishmentController.java
+++ src/main/java/com/wyu/controller/PunishmentController.java
@@ -1,13 +1,14 @@
 package com.wyu.controller;
 
+import java.util.HashMap;
 import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.google.gson.Gson;
 import com.wyu.entity.Punishment;
 import com.wyu.service.PunishmentService;
 import com.wyu.utils.WriteBackUtil;
@@ -44,11 +45,13 @@ public class PunishmentController {
 	}
 
 	@GetMapping("/punishment/query")
-	public WriteBack<List<Punishment>> queryByName(HttpServletRequest request) {
+	public WriteBack<List<Punishment>> queryByName(@RequestBody String body) {
 		WriteBack<List<Punishment>> writeBack = new WriteBack<>();
-		String name = request.getParameter("name");
-		int currentPage = Integer.valueOf(request.getParameter("currentPage"));
-		int size = Integer.valueOf(request.getParameter("size"));
+		Gson gson = new Gson();
+		HashMap<String, Object> hashMap = gson.fromJson(body, HashMap.class);
+		String name = String.valueOf(hashMap.get("name"));
+		int currentPage = ((Double) hashMap.get("currentPage")).intValue();
+		int size = ((Double) hashMap.get("size")).intValue();
 		try {
 			List<Punishment> list = punishmentService.FuzzyqueryByNickName(name, currentPage, size);
 			writeBack.setData(list);
